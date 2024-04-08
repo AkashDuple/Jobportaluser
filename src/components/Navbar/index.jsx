9b5ab2245b6f360bc103eb1b9e414bf4be3561cd
AkashDuple <akash.dupleit@gmail.com>
2024-04-08 18:43:32 +0530

    newchanges

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 function Navbar() {
   return (
-    <div className="h-[100px]">
+    <div style={{ zIndex: 100 }} className="h-[100px]">
       <div className="h-20 p-10 flex items-center fixed w-full z-100 py-15 text-black bg-slate-200">
       <div className="text-4xl pl-20 font-bold"><Link to='/'>Joboard.</Link></div>
     </div>
--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,13 +1,32 @@
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid'
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
 
 const items = [
-  { id: 1, title: 'Back End Developer', department: 'Engineering', type: 'Full-time', location: 'Remote' },
-  { id: 2, title: 'Front End Developer', department: 'Engineering', type: 'Full-time', location: 'Remote' },
-  { id: 3, title: 'User Interface Designer', department: 'Design', type: 'Full-time', location: 'Remote' },
-]
+  {
+    id: 1,
+    title: "Back End Developer",
+    department: "Engineering",
+    type: "Full-time",
+    location: "Remote",
+  },
+  {
+    id: 2,
+    title: "Front End Developer",
+    department: "Engineering",
+    type: "Full-time",
+    location: "Remote",
+  },
+  {
+    id: 3,
+    title: "User Interface Designer",
+    department: "Design",
+    type: "Full-time",
+    location: "Remote",
+  },
+];
+
+const pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 export default function Pagination(props) {
-    
   return (
     <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
       <div className="flex flex-1 justify-between sm:hidden">
@@ -27,21 +46,52 @@ export default function Pagination(props) {
       <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-center">
         <div>
           <p className="text-sm text-gray-700">
-            Showing <span className="font-medium">1</span> to <span className="font-medium">10</span> of{' '}
+            Showing <span className="font-medium">1</span> to{" "}
+            <span className="font-medium">10</span> of{" "}
             <span className="font-medium">97</span> results
           </p>
         </div>
         <div>
-          <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
+          <nav
+            className="isolate inline-flex -space-x-px rounded-md shadow-sm"
+            aria-label="Pagination"
+          >
             <a
               href="#"
               className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
             >
               <span className="sr-only">Previous</span>
-              <ChevronLeftIcon className="h-5 w-5" aria-hidden="true"  onClick={props.onLeftclick}/>
+              <ChevronLeftIcon
+                className="h-5 w-5"
+                aria-hidden="true"
+                onClick={props.onLeftclick}
+              />
             </a>
             {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
-            <a
+
+            {pages.map((d) => {
+              if (d == 1) {
+                return (
+                  <a
+                    href="#"
+                    aria-current="page"
+                    className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    {d}
+                  </a>
+                );
+              } else {
+                return (
+                <a
+                  href="#"
+                  className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                >
+                    {d}
+                </a>)
+              }
+            })}
+
+            {/* <a
               href="#"
               aria-current="page"
               className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
@@ -80,17 +130,21 @@ export default function Pagination(props) {
               className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
             >
               10
-            </a>
+            </a> */}
             <a
               href="#"
               className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
             >
               <span className="sr-only">Next</span>
-              <ChevronRightIcon className="h-5 w-5" aria-hidden="true" onClick={props.onRightclick}/>
+              <ChevronRightIcon
+                className="h-5 w-5"
+                aria-hidden="true"
+                onClick={props.onRightclick}
+              />
             </a>
           </nav>
         </div>
       </div>
     </div>
-  )
+  );
 }
